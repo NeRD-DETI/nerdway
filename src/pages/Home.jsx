d1670e4ff305e86afdd596e61c2606235629defc
Hugo Leal <hugolardosa@gmail.com>
2023-05-08 15:55:25 +0100

    first work

---
+++ src/pages/Home.jsx
@@ -0,0 +1,185 @@
+import React, { useState, useEffect } from "react";
+
+import Helmet from "../components/Helmet/Helmet.js";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+import "../styles/hero-section.css";
+
+import { Link } from "react-router-dom";
+
+import "../styles/home.css";
+
+import products from "../assets/fake-data/products.js";
+
+import foodCategoryImg01 from "../assets/images/hamburger.png";
+import foodCategoryImg02 from "../assets/images/pizza.png";
+import foodCategoryImg03 from "../assets/images/bread.png";
+
+import heroImg from "../assets/images/hero.jpg";
+
+import ProductCard from "../components/UI/product-card/ProductCard.jsx";
+
+
+const Home = () => {
+  const [category, setCategory] = useState("ALL");
+  const [allProducts, setAllProducts] = useState(products);
+
+  const [hotPizza, setHotPizza] = useState([]);
+
+  useEffect(() => {
+    const filteredPizza = products.filter((item) => item.category === "Pizza");
+    const slicePizza = filteredPizza.slice(0, 4);
+    setHotPizza(slicePizza);
+  }, []);
+
+  useEffect(() => {
+    if (category === "ALL") {
+      setAllProducts(products);
+    }
+
+    if (category === "BURGER") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "Burger"
+      );
+
+      setAllProducts(filteredProducts);
+    }
+
+    if (category === "PIZZA") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "Pizza"
+      );
+
+      setAllProducts(filteredProducts);
+    }
+
+    if (category === "BREAD") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "Bread"
+      );
+
+      setAllProducts(filteredProducts);
+    }
+  }, [category]);
+
+  return (
+    <Helmet title="Home">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content  ">
+                <h5 className="mb-3">A maneira mais fácil de comprar snacks no nerd</h5>
+                <h1 className="mb-4 hero__title">
+                  <span>Sem moedas ou notas?</span>
+                  <br/>
+                  Compra por aqui! é só carregar e comprar.
+                </h1>
+
+                <p>
+                  O NerdWay é a plataforma que te permite usar saldo digital para comprares o que quiseres no NeRD!
+                  Assim não precisas de usar mais dinheiuro vivo! Carrega a tua conta e podes comprar à vontade :)
+                </p>
+
+                <div className="hero__btns d-flex align-items-center gap-5 mt-4">
+                  <button className="order__btn d-flex align-items-center justify-content-between">
+                    <Link to="/foods">Comprar snacks <i class="ri-arrow-right-s-line"></i> </Link>
+                  </button>
+
+                  <button className="all__foods-btn">
+                    <Link to="/charge">Carrega saldo</Link>
+                  </button>
+                </div>
+
+                <div className=" hero__service  d-flex align-items-center gap-5 mt-5 ">
+                  <p className=" d-flex align-items-center gap-2 ">
+                    <span className="shipping__icon">
+                      <i class="ri-car-line"></i>
+                    </span>{" "}
+                    No shipping charge
+                  </p>
+
+                  <p className=" d-flex align-items-center gap-2 ">
+                    <span className="shipping__icon">
+                      <i class="ri-shield-check-line"></i>
+                    </span>{" "}
+                    Completamente interno
+                  </p>
+                </div>
+              </div>
+            </Col>
+            {/* Imagem lateral */}
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="hero-img" className="w-100"/>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2>Popular Foods</h2>
+            </Col>
+
+            <Col lg="12">
+              <div className="food__category d-flex align-items-center justify-content-center gap-4">
+                <button
+                  className={`all__btn  ${
+                    category === "ALL" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("ALL")}
+                >
+                  All
+                </button>
+                <button
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "BURGER" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("BURGER")}
+                >
+                  <img src={foodCategoryImg01} alt="" />
+                  Burger
+                </button>
+
+                <button
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "PIZZA" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("PIZZA")}
+                >
+                  <img src={foodCategoryImg02} alt="" />
+                  Pizza
+                </button>
+
+                <button
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "BREAD" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("BREAD")}
+                >
+                  <img src={foodCategoryImg03} alt="" />
+                  Bread
+                </button>
+              </div>
+            </Col>
+
+            {allProducts.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mt-5">
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+
+      
+    </Helmet>
+  );
+};
+
+export default Home;
